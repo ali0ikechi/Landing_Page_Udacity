ebd035a90a2e0ad026566beaebb253f9c7a9eb84
arisa <47748473+ali0ikechi@users.noreply.github.com>
2020-07-24 15:43:28 +0200

    spyscrolling updated

--- js/app.js
+++ js/app.js
@@ -1,18 +1,3 @@
-//Scroll Bar
-
-window.onscroll = function () {
-  myFunction();
-};
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
-
 //Scroll to Sections
 const scrollSmooth = () => {
   const navLinks = document.querySelectorAll(".menu_link");
@@ -30,3 +15,41 @@ const scrollSmooth = () => {
 };
 
 scrollSmooth();
+
+//Scroll bar
+window.onscroll = function () {
+  scrollBar();
+};
+
+function scrollBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+//Back to home
+
+function homeBack() {
+  const home = document.querySelector("#homeButton");
+  home.addEventListener("click");
+}
+
+//SpyScrolling
+
+const links = document.querySelectorAll(".menu_link");
+const sections = document.querySelectorAll(".active-section");
+
+function SpyScrolling() {
+  let index = sections.length;
+
+  while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+  links.forEach((link) => link.classList.remove("active"));
+  links[index].classList.add("active");
+}
+
+SpyScrolling();
+window.addEventListener("scroll", SpyScrolling);
